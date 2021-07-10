d21739066c9b0c6183ebc81e4751ebc857b6745e
Yagnesh13 <55800448+Yagnesh13@users.noreply.github.com>
2021-07-10 17:04:18 +0530

    Update Product_Info.java

--- Product_Info.java
+++ Product_Info.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package shop_manage;
 
-/**
- *
- * @author yagne
- */
 import net.proteanit.sql.DbUtils;
 import java.awt.*;
 import java.awt.event.*;
