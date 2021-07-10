4fef5488bf1c379f27f5e89f7b8224d64c2b666e
Yagnesh13 <55800448+Yagnesh13@users.noreply.github.com>
2021-07-10 17:05:17 +0530

    Update conn.java

--- conn.java
+++ conn.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package shop_manage;
 
 
 import java.sql.*;  
 
-/**
- *
- * @author yagne
- */
+
 public class conn {
     Connection c;
     Statement s;
